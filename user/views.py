d4be4491957754be0525b2679513e84f5a4e104b
ClarkCh <55502622+ClarkCh@users.noreply.github.com>
2019-11-22 13:42:07 +0800

    Update views.py

--- user/views.py
+++ user/views.py
@@ -1,5 +1,5 @@
 import redis
-
+import mysql
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
@@ -7,28 +7,38 @@ from django.http import HttpResponseRedirect
 from .models import Url
 
 
-pool = redis.ConnectionPool(host='127.0.0.1', port=6379, db=2)
+pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
 r = redis.Redis(connection_pool=pool)
 
 
 class IndexView(View):
+    '''首页'''
     def get(self, request):
+        # 首页
         return render(request, 'index.html', {
             'short_url': ''
         })
 
     def post(self, request):
+        # 把传入的上网址分配一个id，并存入mysql
         long_url = request.POST.get('long_url', '')
         if long_url:
             index = int(r.incr(0))
-            r.set(name=index, value=long_url)
-            short_url = 'http://182.61.150.93:82/' + str(index)
-            return render(request, 'index.html', {
-                'short_url': short_url
-            })
+            locked = r.set('string_locked%s'%index, 1, ex=30, nx=2)
+            if locked:
+                r.set(name=index, value=long_url)
+                short_url = 'http://182.61.150.93:82/' + str(index)
+                r.lpush('queue', index, long_url)
+                r.delete('string_locked%s'%index)
+                return render(request, 'index.html', {
+                    'short_url': short_url
+                })
+            else:
+                return render(request, 'failure.html')
 
 
 class UrlView(View):
+    '''根据传入的参数查找出长网址，为避免缓存穿透，在查找不存在的key时会在缓存中记录为空，直至被修改'''
     def get(self, request, index):
         if index:
             long_url = r.get(index)
@@ -54,16 +64,16 @@ class UrlView(View):
 
 
 def play():
-    for t in r.keys():
-        if t == b'0':
-            continue
-        try:
-            start = Url.objects.get(id=t.decode('utf-8'))
-        except:
-            start = Url()
-            start.id = t.decode('utf-8')
-        start.url = r.get(t).decode('utf-8')
-        start.save()
-
-
-
+    lenn = r.llen('list')
+    if lenn:
+        for i in range(int(lenn/2)):
+            id = r.rpop('list')
+            url = r.rpop('list').decode('utf-8')
+            # 这里应该有mysql的消息队列可以用才好
+            try:
+                status = Url.objects.get(id=t.decode('utf-8'))
+            except:
+                status = Url()
+                status.id = id
+            status.url = url
+            status.save()
